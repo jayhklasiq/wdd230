0e5b084e18891535f4d41f7f12cee0dcca8722ad
jayhklasiq <klasik@byui.edu>
2023-06-11 01:32:29 +0000

    dark mode added to the home page

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -7,3 +7,54 @@ currentYearElement.textContent = currentYear;
 const lastModifiedElement = document.getElementById("lastModified");
 const lastModifiedDate = document.lastModified;
 lastModifiedElement.textContent = "Last modified: " + lastModifiedDate;
+
+// Th following code controls the hanburger button
+const navBtn = document.querySelector(".nav-menu");
+const menu = document.querySelector('.navigation')
+// Adding a hamburger menu
+navBtn.addEventListener('click', () => {
+	menu.classList.toggle('open');
+    navBtn.classList.toggle('open');
+});
+
+function darkmode(){
+	main.style.background = "#000";
+	main.style.color = "#fff";
+	modeButton.textContent = "Dark Mode Off";
+}
+
+function lightmode(){
+	main.style.background = "#eee";
+	main.style.color = "#000";
+	modeButton.textContent = "Dark Mode";
+}
+
+const modeButton = document.querySelector(".mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {  
+  if (modeButton.textContent === "Dark Mode")
+	{
+    darkmode()
+  	} 
+  else if (modeButton.textContent === "Dark Mode Off")
+	{
+    lightmode()
+	}
+});
+
+// modeButton.addEventListener("click", () => {
+// 	if (modeButton.textContent.includes("Dark Mode")) {
+// 		main.style.background = "#000";
+// 		main.style.color = "#fff";
+// 		modeButton.textContent = "Dark Mode Off";
+// 	} 
+//     else{
+// 		main.style.background = "#eee";
+// 		main.style.color = "#000";
+// 		modeButton.textContent = "Dark Mode";
+// 	}
+// });
+
+
+
