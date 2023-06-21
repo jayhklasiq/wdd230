4c3cd357d324e886c0b0a7e269e56a97397dc2d2
jayhklasiq <klasik@byui.edu>
2023-06-21 19:48:52 +0000

    continuation to adjust the commerce page

--- chamber/scripts/chamber-of-commerce.js
+++ chamber/scripts/chamber-of-commerce.js
@@ -16,3 +16,36 @@ navBtn.addEventListener('click', () => {
 	menu.classList.toggle('open');
     navBtn.classList.toggle('open');
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the current date
+    let currentDate = new Date();
+  
+    // Retrieve the last visit date from localStorage
+    let lastVisit = localStorage.getItem('lastVisit');
+  
+    // Set the current date as the last visit date in localStorage
+    localStorage.setItem('lastVisit', currentDate.toISOString());
+  
+    // Calculate the time difference between visits
+    let timeDiff = 0;
+    if (lastVisit) {
+      let prevVisit = new Date(lastVisit);
+      timeDiff = Math.floor((currentDate - prevVisit) / (1000 * 60 * 60 * 24));
+    }
+  
+    // Get the sidebar content element
+    let sidebarContent = document.querySelector('.message');
+  
+    // Display appropriate message based on the time between visits
+    if (timeDiff === 0) {
+      sidebarContent.innerHTML = '<p>Welcome! Let us know if you have any questions.</p>';
+    } else if (timeDiff < 1) {
+      sidebarContent.innerHTML = '<p>Back so soon! Awesome!</p>';
+    } else {
+      let daysText = timeDiff === 1 ? 'day' : 'days';
+      sidebarContent.innerHTML = '<p>You last visited ' + timeDiff + ' ' + daysText + ' ago.</p>';
+    }
+  });
+  
