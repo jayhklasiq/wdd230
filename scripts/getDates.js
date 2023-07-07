498d7233237cbb732daf823ac70c7462b00ac704
jayhklasiq <klasik@byui.edu>
2023-07-07 00:24:17 +0000

    ...

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -66,6 +66,43 @@ if (visitCountElement) {
 
 	// Store the updated visit count in localStorage
 	localStorage.setItem("visitCount", visitCount);
+
+
+
+    const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+let captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=7c71213c74f3ed66fde3c04f0ecec19c';
+
+async function apiFetch(url) {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      displayResults(data);
+    } else {
+      throw new Error("API request failed");
+    }
+  } catch (error) {
+    console.log(error);
+    console.log("There is an error fetching weather data.")
+  }
+}
+
+
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  let desc = data.weather[0].description;
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = `${desc}`;
+}
+
+apiFetch(url);
+
 }
 
 // Check the password field for the same input value
