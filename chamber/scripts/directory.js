ca03919f87a78b6b48bfaea68aab2749189f4560
jayhklasiq <klasik@byui.edu>
2023-07-08 13:53:54 +0000

    ...

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,14 +1,8 @@
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
 
-async function displayMembersView(view) {
-	let members = []; // Load members data from the JSON source
-	try {
-		// Fetch JSON data
-		const response = await fetch('data/members.json');
-		const data = await response.json();
-		console.log(data);
-		members = data.members;
-		renderMembersView(view);
-	} catch (error) {
-		console.error('Error fetching member data:', error);
-	}
+async function getProphetData() {
+	const response = await fetch(url);
+	const data = await response.json();
+	console.table(data);
+	displayProphets(data.prophets);
 }
